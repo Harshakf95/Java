fdc4c38c9873dd184d7362eb22e5fa4557c3de94
Harshakf95 <82674064+Harshakf95@users.noreply.github.com>
2023-07-18 13:18:52 +0000

    Preraing to commit

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,4 @@
 import java.util.*;
-
-import javax.lang.model.util.ElementScanner14;
 public class Calculator 
 {
     public static void main(String[] args) 
@@ -31,5 +29,6 @@ public class Calculator
         }
         else
         System.out.println("Invalid choice!");
-    }    
+    }
+    Scanner.close();    
 }
