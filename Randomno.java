5ce9a736a09b84581af345de4fc35e7b6f83b612
Harshakf95 <82674064+Harshakf95@users.noreply.github.com>
2023-07-18 19:45:10 +0000

    Preparing to commit

--- Randomno.java
+++ Randomno.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class Randomno 
 {
-public static void main(String[] args) 
-{
-Random obj = new Random();
-for(int i=0;i<50;i++)
-{
-float num = obj.nextFloat()*50;
-if(num<50)
-num+=50;
-System.out.println(num); 
-}
-} 
+    public static void main(String[] args) 
+    {
+        Random obj = new Random();
+        for(int i=0;i<50;i++)
+        {
+            float num = obj.nextFloat()*50;
+            if(num<50)
+            num+=50;
+            System.out.println(num); 
+        }
+    } 
 }
