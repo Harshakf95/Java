6eeda709611c23b3b538d876c871688c141b4671
Harshakf95 <82674064+Harshakf95@users.noreply.github.com>
2023-07-18 13:07:21 +0000

    Preparing to rectify

--- Age_Cal.java
+++ Age_Cal.java
@@ -3,10 +3,11 @@ public class Age_Cal
 {
     public static void main(String[] args) 
     {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your birth year:");
-        int birthYear = input.nextInt();
-        int age = 2023 - birthYear;
-        System.out.println("You are " + age + " years old.");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter your birth year:");
+            int birthYear = scanner.nextInt();
+            int age = 2023 - birthYear;
+            System.out.println("You are " + age + " years old.");
+        }
     }
 }
